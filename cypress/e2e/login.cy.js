0e7684a513700b61cbb65ebfcd656954162c6ed4
Rafael Ribon <ribon.rafael@gmail.com>
2023-08-30 21:16:37 -0300

    Criado o teste de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,7 +6,20 @@ describe('Realiza Login ', () => {
     cy.get('#user-name').type('standard_user')
     cy.get('#password').type('secret_sauce')
     cy.get('#login-button').click()
-    cy.contains('.product_label').value('Products')
+     cy.contains('.product_label','Products').should('be.visible')
+
+
+
+  })
+
+  it('Login bloqueado', () => {
+    cy.visit('https://www.saucedemo.com/v1/index.html')
+    cy.get('#user-name').type('locked_out_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('#login-button').click()
+    cy.contains('h3[data-test=error]','Epic').should('be.visible')
+    
+
 
   })
 })
